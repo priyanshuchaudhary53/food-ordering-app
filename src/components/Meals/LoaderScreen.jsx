9dfee70a93ee56143d272c53f60b2eb25fd26c12
priyanshuchaudhary53 <priyanshuchaudhary53@gmail.com>
2023-08-01 01:05:15 +0530

    update homepage loader

--- src/components/Meals/LoaderScreen.jsx
+++ src/components/Meals/LoaderScreen.jsx
@@ -1,22 +1,22 @@
 export const LoaderScreen = () => {
   return (
     <div className="w-full">
-      <div className="flex gap-4">
-        <div className="w-[80%]">
+      <div className="flex flex-wrap md:flex-nowrap gap-4">
+        <div className="w-full md:w-[80%]">
           <div className="w-[200px] h-[55px] bg-gray-100 rounded-md"></div>
           <div className="w-full h-[90px] bg-gray-100 mt-3 rounded-md"></div>
         </div>
-        <div className="w-[20%] flex flex-col items-center justify-center">
+        <div className="w-full md:w-[20%] flex flex-col items-start md:items-center md:justify-center">
           <div className="w-[125px] h-[40px] bg-gray-100 rounded-md"></div>
           <div className="w-[125px] h-[40px] mt-3 bg-gray-100 rounded-full"></div>
         </div>
       </div>
-      <div className="flex w-full gap-4 mt-10">
-        <div className="w-[80%]">
+      <div className="flex flex-wrap md:flex-nowrap gap-4 mt-10">
+        <div className="w-full md:w-[80%]">
           <div className="w-[200px] h-[55px] bg-gray-100 rounded-md"></div>
           <div className="w-full h-[90px] bg-gray-100 mt-3 rounded-md"></div>
         </div>
-        <div className="w-[20%] flex flex-col items-center justify-center">
+        <div className="w-full md:w-[20%] flex flex-col items-start md:items-center justify-center">
           <div className="w-[125px] h-[40px] bg-gray-100 rounded-md"></div>
           <div className="w-[125px] h-[40px] mt-3 bg-gray-100 rounded-full"></div>
         </div>
